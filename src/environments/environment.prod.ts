e50a515886d197bc97c9f2e5c6474f41774a3100
paolo <paolo.convertini@gmial.com>
2023-02-02 16:39:44 +0100

    heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'http://localhost:8080/api/v1/',
+  baseAuthUrl: 'http://localhost:8081/api/v1/',
+  LOGIN: 'login',
+  ORDINI_CLIENTI: 'ordini-clienti',
+  ARTICOLI_BY_NUM_ORDINE: "articoli"
 };
