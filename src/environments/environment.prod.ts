30595570dd77c667f03a57c8209f77a7bb194fec
paolo <paolo.convertini@gmial.com>
2023-02-03 10:33:18 +0100

    env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   baseUrl: 'http://localhost:8080/api/v1/',
-  baseAuthUrl: 'https://2873-2001-b07-a3f-e9b-b4d4-6f7a-b802-e5fb.eu.ngrok.io/api/v1/',
+  baseAuthUrl: 'https://4591-2001-b07-a3f-e9b-b4d4-6f7a-b802-e5fb.eu.ngrok.io/api/v1/',
   LOGIN: 'login',
   ORDINI_CLIENTI: 'ordini-clienti',
   ARTICOLI_BY_NUM_ORDINE: "articoli"
