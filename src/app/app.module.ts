1342b409b1bddd8109ae556566aec3bce5f64cbc
paolo <paolo.convertini@gmial.com>
2023-04-05 16:06:18 +0200

    oaf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
 
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavigationComponent } from './components/navigation/navigation.component';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NavigationComponent} from './components/navigation/navigation.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule, MatIconRegistry} from "@angular/material/icon";
-import { LoginComponent } from './components/login/login.component';
+import {LoginComponent} from './components/login/login.component';
 import {MatCardModule} from "@angular/material/card";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
@@ -17,8 +17,8 @@ import {MatButtonModule} from "@angular/material/button";
 import {MatInputModule} from "@angular/material/input";
 import {JwtModule} from "@auth0/angular-jwt";
 import {MatSnackBarModule} from "@angular/material/snack-bar";
-import { OrdineClienteComponent } from './components/ordine-cliente/ordine-cliente-list/ordine-cliente.component';
-import { ArticoloComponent } from './components/ordine-cliente/articolo/articolo.component';
+import {OrdineClienteComponent} from './components/ordine-cliente/ordine-cliente-list/ordine-cliente.component';
+import {ArticoloComponent} from './components/ordine-cliente/articolo/articolo.component';
 import {MatDialogModule} from "@angular/material/dialog";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import {MatPaginatorModule} from "@angular/material/paginator";
@@ -26,21 +26,19 @@ import {MatTableModule} from "@angular/material/table";
 import {MatCheckboxModule} from "@angular/material/checkbox";
 import {MatTooltipModule} from "@angular/material/tooltip";
 import {SnackbarComponent} from "./components/snackbar/snackbar.component";
-import { FirmaDialogComponent } from './components/firma-dialog/firma-dialog.component';
+import {FirmaDialogComponent} from './components/firma-dialog/firma-dialog.component';
 import {RouteReuseStrategy} from "@angular/router";
 import {CustomRouteReuseStrategy} from "./providers/CustomRouteReuseStrategy";
-import { InviaEmailComponent } from './components/invia-email/invia-email.component';
-import { HistoryDialogComponent } from './components/history-dialog/history-dialog.component';
+import {InviaEmailComponent} from './components/invia-email/invia-email.component';
+import {HistoryDialogComponent} from './components/history-dialog/history-dialog.component';
 import {MatGridListModule} from "@angular/material/grid-list";
 import {MatRadioModule} from "@angular/material/radio";
-import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
+import {ConfirmDialogComponent} from './components/confirm-dialog/confirm-dialog.component';
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatMenuModule} from "@angular/material/menu";
-import { OafListComponent } from './components/ordine-fornitore/oaf-list/oaf-list.component';
-import { OafDettaglioComponent } from './components/ordine-fornitore/oaf-dettaglio/oaf-dettaglio.component';
-import {environment} from "../environments/environment";
+import {OafListComponent} from './components/ordine-fornitore/oaf-list/oaf-list.component';
+import {OafDettaglioComponent} from './components/ordine-fornitore/oaf-dettaglio/oaf-dettaglio.component';
 
-const domainArray = environment.allowedDomains;
 
 export function tokenGetter() {
   return localStorage.getItem("access_token");
