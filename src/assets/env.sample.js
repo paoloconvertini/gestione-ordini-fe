8b6f4458fb77a17e67bf8ab1cd8b7370bfa58c42
paolo <paolo.convertini@gmial.com>
2025-09-11 19:13:59 +0200

    docker conf

--- src/assets/env.sample.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-  window.env = window.env || {};    // Environment variables
-  window['env']['baseUrl'] = '${BACKEND_BASE_URL}';
-  window["env"]['baseUrlCeglie'] = '${BACKEND_BASE_C_URL}';
-  window["env"]['baseUrlOstuni'] = '${BACKEND_BASE_O_URL}';
-  window['env']['baseAuthUrl'] = '${BACKEND_BASE_AUTH_URL}';
-  //window['env']['dbxAccessToken'] = '${DBX_ACCESS_TOKEN}';
-})(this);
